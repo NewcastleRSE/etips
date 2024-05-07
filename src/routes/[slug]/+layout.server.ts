c6a287e2525763c6ccf1e75f2ec11d1b77ff7377
Arturo Guzmán Pérez <git@arturoguzman.art>
2024-05-07 13:54:30 +0100

    feat: redirect restricted if cookies are not set

--- src/routes/[slug]/+layout.server.ts
+++ src/routes/[slug]/+layout.server.ts
@@ -1,8 +1,9 @@
 import type { Page } from '$lib/types'
 import { readItems } from '@directus/sdk'
 import type { LayoutServerLoad } from '../$types'
+import { redirect } from '@sveltejs/kit'
 
-export const load: LayoutServerLoad = async ({ locals, params }) => {
+export const load: LayoutServerLoad = async ({ locals, params, cookies }) => {
 	const { directus } = locals
 	const page = await directus.request(
 		readItems('pages', {
@@ -14,6 +15,14 @@ export const load: LayoutServerLoad = async ({ locals, params }) => {
 			]
 		})
 	)
+	if (
+		page[0].category === 'restricted' &&
+		(!cookies.get('etips-side') ||
+			!cookies.get('etips-role') ||
+			!cookies.get('etips-disclaimer-consent'))
+	) {
+		redirect(307, '/access')
+	}
 	const topics = await directus.request(
 		readItems('topics', {
 			fields: ['*', { category: ['title', 'slug'] }, { cards: [{ cards_id: ['*'] }] }],
