3e87a8d311d41a2f7283d6cc566a6b64bbe5f53b
Arturo Guzmán Pérez <git@arturoguzman.art>
2024-06-12 13:47:24 +0100

    refactor: extended logs

--- src/app.d.ts
+++ src/app.d.ts
@@ -16,6 +16,7 @@ declare global {
 				side: string
 				role: string
 			}
+			startTimer: number
 		}
 		// interface PageData {}
 		// interface PageState {}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,30 @@
 import { directus } from '$lib/directus'
-import { getId } from '@arturoguzman/art-ui'
+import { getId, log } from '@arturoguzman/art-ui'
 import type { Handle } from '@sveltejs/kit'
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const id = getId()
-	console.time(`id: ${id}`)
+	// const id = getId()
+	// console.time(`id: ${id}`)
+	event.locals.startTimer = Date.now()
 	event.locals.directus = directus
 	event.locals.session = {
 		side: event.cookies.get('etips-side') ?? 'none',
 		role: event.cookies.get('etips-role') ?? 'public'
 	}
 	const response = await resolve(event)
-	console.timeEnd(`id: ${id}`)
+	if (!event.url.pathname.includes('/assets')) {
+		log({ response: response, event: event, status: response.status })
+	}
+	// console.timeEnd(`id: ${id}`)
 	return response
+}
+
+export const handleError = async ({ event, status, message }) => {
+	// const errorId = crypto.randomUUID()
+	log({ status: status, event: event, content: message })
+
+	return {
+		message: status === 404 ? `This page does not exist!` : 'Whoops!'
+		// errorId
+	}
 }
