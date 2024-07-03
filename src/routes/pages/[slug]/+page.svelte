2961c5afe115957b5091310c41a8f85e5c0be00a
Arturo Guzmán Pérez <git@arturoguzman.art>
2024-07-03 08:39:09 +0100

    fix: scroll on narrow screens

--- src/routes/pages/[slug]/+page.svelte
+++ src/routes/pages/[slug]/+page.svelte
@@ -10,14 +10,9 @@
 		<title>{genTitle([$page.params.slug, $page.params.topic])}</title>
 	{/key}
 </svelte:head>
-<!-- HACK: to get scroll for now -->
-<div class="content-container min-h-screen overflow-x-hidden lg:w-full">
-	<!-- <pre>{JSON.stringify(data.page, null, 2)}</pre> -->
+<div class="content-container h-full overflow-x-hidden lg:w-full">
 	{#if data.page.cards}
 		<CardsContainer cards={data.page.cards}></CardsContainer>
-
-		<!-- <CardsContainer display={data.topic.display ?? 'cards'} cards={data.topic.cards} -->
-		<!-- ></CardsContainer> -->
 	{/if}
 </div>
 
@@ -26,8 +21,4 @@
 		width: min(100% - 4rem, 800px);
 		margin-inline: auto;
 	}
-	/* .cards { */
-	/* 	border-right: 1px solid var(--theme-colour-4); */
-	/* 	border-left: 1px solid var(--theme-colour-4); */
-	/* } */
 </style>
